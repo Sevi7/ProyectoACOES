10fe6efe75ae8d42950e304f9e41f45fb8d4d309
Sevi7 <adrisevilla98@gmail.com>
2018-12-01 19:59:51 +0100

    Añadido src y gitignore

---
+++ src/ProyectoACOES/BDLibrary.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace BDLibrary
+{
+    public class BDLibrary
+    {
+        public string origen_datos;
+        public string cadenaConexion;
+        public string cadenaConexionSQLNCLI;
+
+        public BDLibrary()
+        {
+            
+        }
+
+        public List<object[]> Select(string consultaSelect)
+        {
+            
+            if (consultaSelect.Length == 0) return null;
+            if (consultaSelect.ToUpper().IndexOf("SELECT") == -1) return null;
+            if (consultaSelect.ToUpper().IndexOf("DROP") > -1) return null;
+
+            List<object[]> res = new List<object[]>();
+            OleDbConnection conexion = new OleDbConnection(cadenaConexion);
+            OleDbCommand comando = new OleDbCommand(consultaSelect, conexion);
+
+            conexion.Open();
+            System.Data.Common.DbDataReader reader = comando.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int numColumnas = reader.FieldCount;
+                object[] fila = new object[numColumnas];
+                for (int i = 0; i < numColumnas; ++i) fila[i] = reader[i];
+                res.Add(fila);
+            }
+
+            conexion.Close();
+
+            
+            return res;
+        }
+
+        public object SelectScalar(string consultaSelect)
+        {
+
+            if (consultaSelect.Length == 0) return null;
+            if (consultaSelect.ToUpper().IndexOf("SELECT") == -1) return null;
+            if (consultaSelect.ToUpper().IndexOf("DROP") > -1) return null;
+
+          
+            OleDbConnection conexion = new OleDbConnection(cadenaConexion);
+            OleDbCommand comando = new OleDbCommand(consultaSelect, conexion);
+
+            conexion.Open();
+            object res = comando.ExecuteScalar();
+
+            conexion.Close();
+
+
+            return res;
+        }
+
+        public void Update(string cadenaUpdate)
+        {
+            if (cadenaUpdate.Length == 0) return;
+
+            OleDbConnection conexion = new OleDbConnection(cadenaConexion);
+            OleDbCommand comando = new OleDbCommand(cadenaUpdate, conexion);
+
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void Delete(string cadenaDelete)
+        {
+            if (cadenaDelete.Length == 0) return;
+
+            OleDbConnection conexion = new OleDbConnection(cadenaConexion);
+            OleDbCommand comando = new OleDbCommand(cadenaDelete, conexion);
+
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void Insert(string cadenaInsert)
+        {
+            if (cadenaInsert.Length == 0) return;
+
+            OleDbConnection conexion = new OleDbConnection(cadenaConexion);
+
+            OleDbCommand comando = new OleDbCommand(cadenaInsert, conexion);
+
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void EjecutaProcedimiento(string name, SqlParameter[] param)
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexionSQLNCLI);
+            SqlCommand comando = new SqlCommand(name, conexion);
+            foreach (SqlParameter p in param)
+            {
+                comando.Parameters.Add(p);
+            }
+            comando.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+    }
+
+    public class AccessDB : BDLibrary
+    {
+        public AccessDB(string pathBD)
+        {
+            origen_datos = pathBD;
+            cadenaConexion = "Provider=Microsoft.Jet.OLEDB.4.0; "
+                    + "Data Source=" + pathBD;
+        }
+
+        public AccessDB(string pathBD, string pwd)
+        {
+            origen_datos = pathBD;
+            cadenaConexion = "Provider=Microsoft.Jet.OLEDB.4.0; "
+                    + "Data Source=" + pathBD
+                    + ";Jet OLEDB:Database Password=" + pwd;
+        }
+    }
+
+    public class AccessDB2007 : BDLibrary
+    {
+
+        public AccessDB2007(string pathBD)
+        {
+            origen_datos = pathBD;
+            cadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0; "
+                    + "Data Source=" + pathBD;
+        }
+
+        public AccessDB2007(string pathBD, string pwd)
+        {
+            origen_datos = pathBD;
+            cadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0; "
+                    + "Data Source=" + pathBD
+                    + ";Jet OLEDB:Database Password=" + pwd;
+        }
+    }
+
+    public class SQLSERVERDB : BDLibrary
+    {
+        public SQLSERVERDB(string server, string instancia, string BD)
+        {
+            origen_datos = BD;
+
+            cadenaConexion = "Provider=SQLOLEDB;Data Source=" + server + "\\" + instancia
+                + ";Integrated Security=SSPI;Initial Catalog=" + BD;
+            cadenaConexionSQLNCLI = "Data Source=" + server + "\\" + instancia
+                + ";Integrated Security=SSPI;Initial Catalog=" + BD;
+        }
+
+        public SQLSERVERDB(string server, string BD)
+        {
+            origen_datos = BD;
+
+            cadenaConexion = "Provider=SQLOLEDB;Data Source=" + server + ";"
+                + "Integrated Security=SSPI;Initial Catalog=" + BD;
+            cadenaConexionSQLNCLI = "Data Source=" + server + ";"
+                + "Integrated Security=SSPI;Initial Catalog=" + BD;
+        }
+
+
+        public SQLSERVERDB(string server, string instancia, string BD, string user, string pwd)
+        {
+            origen_datos = BD;
+
+            cadenaConexion = "Provider=SQLOLEDB;Data Source=" + server + "\\" + instancia
+                + "Initial Catalog=" + BD + ";User ID=" + user + ";Password=" + pwd + ";";
+            cadenaConexionSQLNCLI = "Data Source=" + server + "\\" + instancia
+                + "Initial Catalog=" + BD + ";User ID=" + user + ";Password=" + pwd + ";";
+        }
+
+        public SQLSERVERDB(string server, string BD, string user, string pwd)
+        {
+            origen_datos = BD;
+
+            cadenaConexion = "Provider=SQLOLEDB;Data Source=" + server + ";"
+                + "Initial Catalog=" + BD + ";User ID=" + user + ";Password=" + pwd + ";";
+            cadenaConexionSQLNCLI = "Data Source=" + server + ";"
+                + "Initial Catalog=" + BD + ";User ID=" + user + ";Password=" + pwd + ";";
+        }
+
+
+
+        
+
+    }
+
+
+}
---
+++ src/ProyectoACOES/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoACOES
+{
+    class Error : Exception
+    {
+        public Error() : base() { }
+        public Error(string message) : base(message) { }
+    }
+}

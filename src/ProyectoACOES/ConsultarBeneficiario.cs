c352ae1b23cf6a100ff1446dca97d9958794aed7
Dinohaurio <36729707+Dinohaurio@users.noreply.github.com>
2018-12-04 00:51:43 +0100

    Control errores

--- src/ProyectoACOES/ConsultarBeneficiario.cs
+++ src/ProyectoACOES/ConsultarBeneficiario.cs
@@ -45,24 +45,30 @@ namespace ProyectoACOES
 
         private void button2_Click(object sender, EventArgs e)
         {
-            beneficiario = new Beneficiario(Convert.ToInt32(tCodigo.Text));
-            textBox1.Text = beneficiario.nombre_Beneficiario;
-            textBox2.Text = beneficiario.apellidos_Beneficiario;
-            textBox3.Text = beneficiario.estado_Beneficiario;
-            textBox4.Text = beneficiario.sexo_Beneficiario;
-            textBox5.Text = beneficiario.beca_Beneficiario;
-            textBox6.Text = beneficiario.curso_Beneficiario;
-            textBox7.Text = beneficiario.proyecto_Beneficiario;
-            textBox8.Text = Convert.ToString(beneficiario.agente_Beneficiario.Codigo);
-            textBox9.Text = beneficiario.comunidadAct_Beneficiario;
-            textBox10.Text = beneficiario.comunidadProc_Beneficiario;
-            textBox11.Text = beneficiario.observaciones_Beneficiario;
-            dateTimePicker1.Value = beneficiario.fechaNacimiento_Beneficiario;
-            dateTimePicker2.Value = beneficiario.fechaEntradaAcoes_Beneficiario;
-            dateTimePicker3.Value = beneficiario.fechaEntradaProyecto_Beneficiario;
-            dateTimePicker4.Value = beneficiario.fechaSalidaProyecto_Beneficiario;
-            dateTimePicker5.Value = beneficiario.fechaAlta_Beneficiario;
-            dateTimePicker6.Value = beneficiario.fechaSalidaAcoes_Beneficiario;
-        }
+            try{
+                beneficiario = new Beneficiario(Convert.ToInt32(tCodigo.Text));
+                textBox1.Text = beneficiario.nombre_Beneficiario;
+                textBox2.Text = beneficiario.apellidos_Beneficiario;
+                textBox3.Text = beneficiario.estado_Beneficiario;
+                textBox4.Text = beneficiario.sexo_Beneficiario;
+                textBox5.Text = beneficiario.beca_Beneficiario;
+                textBox6.Text = beneficiario.curso_Beneficiario;
+                textBox7.Text = beneficiario.proyecto_Beneficiario;
+                textBox8.Text = Convert.ToString(beneficiario.agente_Beneficiario.Codigo);
+                textBox9.Text = beneficiario.comunidadAct_Beneficiario;
+                textBox10.Text = beneficiario.comunidadProc_Beneficiario;
+                textBox11.Text = beneficiario.observaciones_Beneficiario;
+                dateTimePicker1.Value = beneficiario.fechaNacimiento_Beneficiario;
+                dateTimePicker2.Value = beneficiario.fechaEntradaAcoes_Beneficiario;
+                dateTimePicker3.Value = beneficiario.fechaEntradaProyecto_Beneficiario;
+                dateTimePicker4.Value = beneficiario.fechaSalidaProyecto_Beneficiario;
+                dateTimePicker5.Value = beneficiario.fechaAlta_Beneficiario;
+                dateTimePicker6.Value = beneficiario.fechaSalidaAcoes_Beneficiario;
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+                            
+            }
     }
 }

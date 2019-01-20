9b4e3f998420ddc05de5d70fbe38cfbb67796822
Dinohaurio <kilojulio@gmail.com>
2019-01-20 20:09:48 +0100

    Beneficiario
    pos eso

---
+++ src/ProyectoACOES/GestionBeneficiario.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace ProyectoACOES
+{
+    partial class GestionBeneficiario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tNIF = new System.Windows.Forms.Label();
+            this.tNombre = new System.Windows.Forms.Label();
+            this.tApellidos = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nIFDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellidosDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.observacionesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.responsableDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numCuentaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.beneficiarioBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.aCOESDataSet = new ProyectoACOES.ACOESDataSet();
+            this.tDireccion = new System.Windows.Forms.Label();
+            this.tNumeroCuenta = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tObservaciones = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.beneficiarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.beneficiarioTableAdapter = new ProyectoACOES.ACOESDataSetTableAdapters.BeneficiarioTableAdapter();
+            this.beneficiarioBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beneficiarioBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aCOESDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beneficiarioBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beneficiarioBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(240, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Beneficiarios";
+            // 
+            // tNIF
+            // 
+            this.tNIF.AutoSize = true;
+            this.tNIF.Location = new System.Drawing.Point(50, 282);
+            this.tNIF.Name = "tNIF";
+            this.tNIF.Size = new System.Drawing.Size(24, 13);
+            this.tNIF.TabIndex = 2;
+            this.tNIF.Text = "NIF";
+            // 
+            // tNombre
+            // 
+            this.tNombre.AutoSize = true;
+            this.tNombre.Location = new System.Drawing.Point(50, 312);
+            this.tNombre.Name = "tNombre";
+            this.tNombre.Size = new System.Drawing.Size(44, 13);
+            this.tNombre.TabIndex = 3;
+            this.tNombre.Text = "Nombre";
+            // 
+            // tApellidos
+            // 
+            this.tApellidos.AutoSize = true;
+            this.tApellidos.Location = new System.Drawing.Point(50, 341);
+            this.tApellidos.Name = "tApellidos";
+            this.tApellidos.Size = new System.Drawing.Size(49, 13);
+            this.tApellidos.TabIndex = 4;
+            this.tApellidos.Text = "Apellidos";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nIFDataGridViewTextBoxColumn,
+            this.nombreDataGridViewTextBoxColumn,
+            this.apellidosDataGridViewTextBoxColumn,
+            this.observacionesDataGridViewTextBoxColumn,
+            this.responsableDataGridViewTextBoxColumn,
+            this.numCuentaDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.beneficiarioBindingSource2;
+            this.dataGridView1.Location = new System.Drawing.Point(53, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(642, 148);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // nIFDataGridViewTextBoxColumn
+            // 
+            this.nIFDataGridViewTextBoxColumn.DataPropertyName = "NIF";
+            this.nIFDataGridViewTextBoxColumn.HeaderText = "NIF";
+            this.nIFDataGridViewTextBoxColumn.Name = "nIFDataGridViewTextBoxColumn";
+            // 
+            // nombreDataGridViewTextBoxColumn
+            // 
+            this.nombreDataGridViewTextBoxColumn.DataPropertyName = "nombre";
+            this.nombreDataGridViewTextBoxColumn.HeaderText = "nombre";
+            this.nombreDataGridViewTextBoxColumn.Name = "nombreDataGridViewTextBoxColumn";
+            // 
+            // apellidosDataGridViewTextBoxColumn
+            // 
+            this.apellidosDataGridViewTextBoxColumn.DataPropertyName = "apellidos";
+            this.apellidosDataGridViewTextBoxColumn.HeaderText = "apellidos";
+            this.apellidosDataGridViewTextBoxColumn.Name = "apellidosDataGridViewTextBoxColumn";
+            // 
+            // observacionesDataGridViewTextBoxColumn
+            // 
+            this.observacionesDataGridViewTextBoxColumn.DataPropertyName = "observaciones";
+            this.observacionesDataGridViewTextBoxColumn.HeaderText = "observaciones";
+            this.observacionesDataGridViewTextBoxColumn.Name = "observacionesDataGridViewTextBoxColumn";
+            // 
+            // responsableDataGridViewTextBoxColumn
+            // 
+            this.responsableDataGridViewTextBoxColumn.DataPropertyName = "responsable";
+            this.responsableDataGridViewTextBoxColumn.HeaderText = "responsable";
+            this.responsableDataGridViewTextBoxColumn.Name = "responsableDataGridViewTextBoxColumn";
+            // 
+            // numCuentaDataGridViewTextBoxColumn
+            // 
+            this.numCuentaDataGridViewTextBoxColumn.DataPropertyName = "numCuenta";
+            this.numCuentaDataGridViewTextBoxColumn.HeaderText = "numCuenta";
+            this.numCuentaDataGridViewTextBoxColumn.Name = "numCuentaDataGridViewTextBoxColumn";
+            // 
+            // beneficiarioBindingSource2
+            // 
+            this.beneficiarioBindingSource2.DataMember = "Beneficiario";
+            this.beneficiarioBindingSource2.DataSource = this.aCOESDataSet;
+            // 
+            // aCOESDataSet
+            // 
+            this.aCOESDataSet.DataSetName = "ACOESDataSet";
+            this.aCOESDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tDireccion
+            // 
+            this.tDireccion.AutoSize = true;
+            this.tDireccion.Location = new System.Drawing.Point(50, 367);
+            this.tDireccion.Name = "tDireccion";
+            this.tDireccion.Size = new System.Drawing.Size(52, 13);
+            this.tDireccion.TabIndex = 6;
+            this.tDireccion.Text = "Direccion";
+            // 
+            // tNumeroCuenta
+            // 
+            this.tNumeroCuenta.AutoSize = true;
+            this.tNumeroCuenta.Location = new System.Drawing.Point(50, 392);
+            this.tNumeroCuenta.Name = "tNumeroCuenta";
+            this.tNumeroCuenta.Size = new System.Drawing.Size(95, 13);
+            this.tNumeroCuenta.TabIndex = 7;
+            this.tNumeroCuenta.Text = "Numero de cuenta";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 279);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(176, 305);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(176, 333);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(167, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(176, 359);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(167, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(176, 385);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(167, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(377, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Observaciones";
+            // 
+            // tObservaciones
+            // 
+            this.tObservaciones.Location = new System.Drawing.Point(380, 305);
+            this.tObservaciones.Multiline = true;
+            this.tObservaciones.Name = "tObservaciones";
+            this.tObservaciones.Size = new System.Drawing.Size(315, 100);
+            this.tObservaciones.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 420);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Modificar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(442, 420);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(620, 420);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Limpiar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // beneficiarioBindingSource
+            // 
+            this.beneficiarioBindingSource.DataMember = "Beneficiario";
+            this.beneficiarioBindingSource.DataSource = this.aCOESDataSet;
+            // 
+            // beneficiarioTableAdapter
+            // 
+            this.beneficiarioTableAdapter.ClearBeforeFill = true;
+            // 
+            // beneficiarioBindingSource1
+            // 
+            this.beneficiarioBindingSource1.DataMember = "Beneficiario";
+            this.beneficiarioBindingSource1.DataSource = this.aCOESDataSet;
+            // 
+            // GestionBeneficiario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 455);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tObservaciones);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tNumeroCuenta);
+            this.Controls.Add(this.tDireccion);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tApellidos);
+            this.Controls.Add(this.tNombre);
+            this.Controls.Add(this.tNIF);
+            this.Controls.Add(this.label1);
+            this.Name = "GestionBeneficiario";
+            this.Text = "Gestion Beneficiario";
+            this.Load += new System.EventHandler(this.GestionBeneficiario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beneficiarioBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aCOESDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beneficiarioBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beneficiarioBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private ACOESDataSet aCOESDataSet;
+        private System.Windows.Forms.BindingSource beneficiarioBindingSource;
+        private ACOESDataSetTableAdapters.BeneficiarioTableAdapter beneficiarioTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label tNIF;
+        private System.Windows.Forms.Label tNombre;
+        private System.Windows.Forms.Label tApellidos;
+        private System.Windows.Forms.BindingSource beneficiarioBindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nIFDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellidosDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn observacionesDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn responsableDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numCuentaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource beneficiarioBindingSource2;
+        private System.Windows.Forms.Label tDireccion;
+        private System.Windows.Forms.Label tNumeroCuenta;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tObservaciones;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ src/ProyectoACOES/GestionBeneficiario.cs
@@ -0,0 +1,94 @@
+﻿using BDLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoACOES
+{
+    public partial class GestionBeneficiario : Form
+    {
+        private Beneficiario seleccionado;
+        private Usuario usuario;
+        private static string BD_SERVER = Properties.Settings.Default.BD_SERVER;
+        private static string BD_NAME = Properties.Settings.Default.BD_NAME;
+
+        public GestionBeneficiario(Usuario u)
+        {
+            InitializeComponent();
+            usuario = u;
+        }
+
+        private void GestionBeneficiario_Load(object sender, EventArgs e)
+        {
+            SQLSERVERDB miBD = new SQLSERVERDB(BD_SERVER, BD_NAME);
+            dataGridView1.DataSource = miBD.Select("select NIF, Nombre, Apellidos, Responsable, Numero de Cuenta, Observaciones from beneficiario where eliminado !=1");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                seleccionado = new Beneficiario(tNIF.Text, tNIF.Text, tApellidos.Text, tObservaciones.Text, usuario, tNumeroCuenta.Text);
+                SQLSERVERDB miBD = new SQLSERVERDB(BD_SERVER, BD_NAME);
+                dataGridView1.DataSource = miBD.Select("select NIF, Nombre, Apellidos, Responsable, Numero de Cuenta, Observaciones from beneficiario where eliminado !=1");
+                RefrescaDatos();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                seleccionado.BorrarBeneficiario();
+                seleccionado = null;
+                SQLSERVERDB miBD = new SQLSERVERDB(BD_SERVER, BD_NAME);
+                dataGridView1.DataSource = miBD.Select("select NIF, Nombre, Apellidos, Responsable, Numero de Cuenta, Observaciones from beneficiario where eliminado !=1");
+                RefrescaDatos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                string nif = (string)dataGridView1.SelectedRows[0].Cells[0].Value;
+                seleccionado = new Beneficiario(nif);
+                RefrescaDatos();
+            }
+        }
+
+        public void RefrescaDatos()
+        {
+            if (seleccionado != null)
+            {
+                tNIF.Text = seleccionado.NIF;
+                tNombre.Text = seleccionado.Nombre;
+                tApellidos.Text = seleccionado.Apellidos;
+                tNumeroCuenta.Text = seleccionado.NumCuenta;
+                tObservaciones.Text = seleccionado.Observaciones;
+            }
+            else
+            {
+                tNIF.Text = "";
+                tNombre.Text = "";
+                tApellidos.Text = "";
+                tNumeroCuenta.Text = "";
+                tObservaciones.Text = "";
+            }
+        }
+    }
+}
